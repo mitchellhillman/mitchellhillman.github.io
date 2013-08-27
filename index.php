ccc1a35c545a85f306e3b90887a4863146bd765d
Mitchell Hillman <mitchellhillman@gmail.com>
2013-08-27 17:02:04 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include('inc/header.inc'); ?>
+
+	<div class="banner">
+		<ul class="tiles">
+			<li class="logo"></li>
+			<li class="dribbble"><a href="http://dribbble.com/mitchellhillman"></a></li>
+			<li class="twitter"><a href="https://twitter.com/mitchellhillman"></a></li>
+			<li class="linkedin"><a href="http://www.linkedin.com/in/mitchellhillman"></a></li>
+			<li class="github"><a href="https://github.com/mitchellhillman"></a></li>
+			<li class="behance"><a href="http://www.behance.net/mitchellhillman"></a></li>
+			<li class="email"><a href="mailto:mitchellhillman@gmail.com"></a></li>
+		</ul>
+	</div>
+
+	<div class="content home">
+		<h1>Hi, my name is <span>Mitchell Hillman.</span></h1>
+		<p>I am a user experience designer and developer working with <a href="http://www.journeygroup.com/">Journey Group</a> in Charlottesville, Virginia.</p>
+	</div>
+
+<?php include('inc/footer.inc'); ?>
---
+++ page.php
@@ -0,0 +1,40 @@
+<?php include('inc/header.inc'); ?>
+
+<div class="content">
+
+<h1>Heading 1</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur <a href="#">adipisicing</a> elit, sed do <strong>eiusmod tempor incididunt</strong> ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <em>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</em>. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<h2>Heading 2</h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur <a href="#">adipisicing</a> elit, sed do <strong>eiusmod tempor incididunt</strong> ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <em>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</em>. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<ul>
+	<li>Test Driven Development</li>
+	<li>Back End Angst</li>
+	<li>What is a Career?</li>
+</ul>
+
+<ol>
+	<li>Make a plan</li>
+	<li>Execute plan</li>
+	<li>Deliver a product</li>
+</ol>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<blockquote>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<p class="source">&ndash; Tony Soprano, Lord of Flies</p>
+</blockquote>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<h3></h3>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+</div>
+
+<?php include('inc/footer.inc'); ?>
